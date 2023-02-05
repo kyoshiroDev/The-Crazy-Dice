dbf30eb657952c2fd0880b66f813d0d401033634
Kyoshiro laptotdev <tahir.gregory@gmail.com>
2023-02-05 12:16:47 +0100

    modification animation dice

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 /* Le lancé du dé et affichage résultat */
 let theDice = document.querySelector("#dice");
 const elComeOut = document.querySelector("#roll");
@@ -5,17 +7,20 @@ const theRollDice = document.querySelector("#dice");
 
 elComeOut.onclick = function rollDice() {
 	let diceResult = Math.floor(Math.random() * 6 + 1);
+
 	theDice.classList.add("dice-roll");
 	console.log(diceResult);
 
-	setTimeout(function rollDice () {
+	setTimeout(function rollDice() {
 		theDice.classList.remove("dice-roll");
-	}, 4000);
+	}, 1000); 
+
+  for (let i = 1; i <= 6; i++) {
+		if (diceResult === i) {
+			theDice.querySelector("#face-" + i);
+    }
+  } 
+
 };
 
 
-/*for (let i = 1; i <= 6; i++) {
-		if (diceResult === i) {
-			theDice.className = "#face-" + i;
-		}
-	}*/
