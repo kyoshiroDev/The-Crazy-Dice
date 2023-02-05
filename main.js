ceef80ec7401a62967b79222a78450ef6c52b9d3
Kyoshiro laptotdev <tahir.gregory@gmail.com>
2023-02-05 11:26:24 +0100

    le dé tourne

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-const theDice = document.querySelector("the-dice");
-const elComeOut = document.getElementById("roll");
-const theRollDice = document.getElementById("dice");
+/* Le lancé du dé et affichage résultat */
+let theDice = document.querySelector("#dice");
+const elComeOut = document.querySelector("#roll");
+const theRollDice = document.querySelector("#dice");
 
-elComeOut.onclick = function () {
-	rollDice();
+elComeOut.onclick = function rollDice() {
+	let diceResult = Math.floor(Math.random() * 6 + 1);
+	theDice.classList.add("dice-roll");
+	console.log(diceResult);
+
+	setTimeout(function rollDice () {
+		theDice.classList.remove("dice-roll");
+	}, 4000);
 };
 
-function rollDice() {
-	let dice = Math.floor(Math.random() * 6 + 1);
-	console.log(dice);
 
-	for (let i = 1; i <= 6; i++) {
-		theDice.classList.remove("dice-" + i);
-	}
-}
+/*for (let i = 1; i <= 6; i++) {
+		if (diceResult === i) {
+			theDice.className = "#face-" + i;
+		}
+	}*/
